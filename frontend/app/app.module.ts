9198526a10cc7f3cc4806637384630123dfa1b46
Stanislav Křikava <krikavastanislav@gmail.com>
2025-03-12 08:55:05 +0100

    Created project structure

---
+++ frontend/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    UserListComponent,
+    UserFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
